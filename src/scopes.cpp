8acb70e73be783892a19a34b5137e0d1a3bcf429
Leonard Ritter <contact@leonard-ritter.com>
2018-03-01 21:06:07 +0100

    * fixed possible heap corruption

--- src/scopes.cpp
+++ src/scopes.cpp
@@ -17454,9 +17454,9 @@ int main(int argc, char *argv[]) {
             scopes_compiler_path = strdup("");
         }
 
-        char *compilerdir = dirname(strdup(scopes_compiler_path));
-        scopes_compiler_dir = format("%s/..", compilerdir)->data;
-        free(compilerdir);
+        char *path_copy = strdup(scopes_compiler_path);
+        scopes_compiler_dir = format("%s/..", dirname(path_copy))->data;
+        free(path_copy);
         scopes_clang_include_dir = format("%s/lib/clang/include", scopes_compiler_dir)->data;
         scopes_include_dir = format("%s/include", scopes_compiler_dir)->data;
     }
