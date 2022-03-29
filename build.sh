cd9189163a563d3828bfad63f9221057c4d56d64
Leonard Ritter <contact@leonard-ritter.com>
2022-03-29 16:39:54 +0200

    * fixed faulty path in scopes recipe * `./build.sh` autoconfirms package installation

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # g++, make and cmake must be installed on this machine
-./bin/eo install genie clang spirv-tools spirv-cross \
+./bin/eo install -y genie clang spirv-tools spirv-cross \
  && ./bin/genie gmake \
  && make -j$(nproc) -C build config=release
